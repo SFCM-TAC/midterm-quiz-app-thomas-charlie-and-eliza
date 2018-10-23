5be254eab387b24fd812d4699b5296982d7617c9
Thomas <Thomas@Thomass-MacBook-Pro-2.local>
2018-10-22 22:40:56 -0700

    Changed the selected box action to being when mouse is hovered over, not when clicked. Created a seperate error, but working for now.

--- js/quizManipulation.js
+++ js/quizManipulation.js
@@ -30,17 +30,10 @@ function changeBoxColors(color) {
   }
 }
   console.log('Selected color: ' + color);
-  // TODO: Look at styles.css and choose a class
-  // to apply to all of the box elements in order
-  // to change their background color
-  // possible values of color are: 'red', 'blue', 'yellow'
-  // (consider using a switch statement!)
 }
 
 function addBox() {
   console.log('Adding a new box');
-  // TODO: Add a new div with class="box" to
-  // the boxes section of the dom tree
 
   var newBoxElement = document.createElement('div');
     newBoxElement.setAttribute("class", "box");
@@ -52,10 +45,6 @@ function addBox() {
     var position = document.getElementById("boxes");
     position.appendChild(newBoxElement);
 
-   // replace with your code
-  // This line is needed to make sure that new boxes
-  // handle clicks. Make sure thatnewBoxElement refers
-  // to the DOM node containing your new Div.
   newBoxElement.addEventListener('click', handleBoxClick);
 }
 
@@ -66,24 +55,28 @@ function removeSelectedBoxes() {
     parent.removeChild(selectedBoxes[0]);
 }
   console.log('Removing selected boxes');
-  // TODO: look at the selectedBoxes array and remove each of those
-  // from their parent in the DOM tree (their parent is the div with id="boxes").
-  // Think about what happens to the selectedBoxes array when you're done!
 }
 
-/* Event Handlers */
-function handleBoxClick(event) {
+function handleMouseover(event) {
   var boxElement = event.target;
-  if (boxElement.classList.contains("box-selected") === false) {
-    boxElement.classList.add("box-selected");
-  } else if (boxElement.classList.contains("box-selected") === true) {
-    boxElement.classList.remove("box-selected")
-  }
+  boxElement.classList.add("box-selected")
+  // if (boxElement.classList.contains("box-selected") === false) {
+  //   boxElement.classList.add("box-selected");
+  // } else if (boxElement.classList.contains("box-selected") === true) {
+  //   boxElement.classList.remove("box-selected")
+  // }
   console.log('Selecting box: ', boxElement);
-  // TODO: add or remove the box from the array of selectedBoxes
-  // TODO: looke at styles.css and choose a class (or multiple classes)
-  // to apply in order to add a border around selected boxes or
-  // remove a border from deselected boxes
+}
+
+function handleMouseout(event) {
+  var boxElement = event.target;
+  boxElement.classList.remove("box-selected")
+  // if (boxElement.classList.contains("box-selected") === false) {
+  //   boxElement.classList.add("box-selected");
+  // } else if (boxElement.classList.contains("box-selected") === true) {
+  //   boxElement.classList.remove("box-selected")
+  // }
+  console.log('Selecting box: ', boxElement);
 }
 
 function handleColorSelect(event) {
@@ -92,8 +85,11 @@ function handleColorSelect(event) {
 
 function attachListeners() {
   document.querySelectorAll('.box').forEach(function(box){
-    box.addEventListener('click', handleBoxClick);
+    box.addEventListener('mouseover', handleMouseover);
   });
+  document.querySelectorAll('.box').forEach(function(box){
+      box.addEventListener('mouseout', handleMouseout);
+    });
   document.querySelectorAll('.color-selector').forEach(function(color){
     color.addEventListener('click', handleColorSelect);
   });
