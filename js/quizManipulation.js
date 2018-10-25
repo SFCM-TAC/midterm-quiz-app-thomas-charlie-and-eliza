166d1c277512766a23fd5507c5fd7198334bee8e
Charlie <Charlie@Chalies-MacBook.local>
2018-10-24 17:18:57 -0700

    WORK!!

--- js/quizManipulation.js
+++ js/quizManipulation.js
@@ -1,6 +1,20 @@
 
 var newColor = ""
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
 function changeBoxColors(color) {
   var boxClass = document.getElementsByClassName("box")
   if (boxClass.length > 1) {
